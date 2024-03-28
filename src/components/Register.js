5e772433e6dc8e5aff7b349367416f774e8516b8
sachinxc <160459008+sachinxc@users.noreply.github.com>
2024-03-28 18:46:51 +0530

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -97,7 +97,7 @@ const Register = () => {
     } else if (step === 2) {
       // Validate if all necessary fields are filled in step 2
       if (
-        (formData.accountType !== "individual" &&
+        (formData.accountType === "" &&
           formData.interests.length < 6) || // Individual account type
         (formData.accountType !== "organization" && // Organization account type
           formData.interests.length < 6) // Interests selected
