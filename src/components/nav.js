5b401ae0365bc9cb9da870d09475c531de39ba67
sachinxc <sachincm10@gmail.com>
2024-03-29 16:39:34 +0530

    nav code simplified

--- src/components/nav.js
+++ src/components/nav.js
@@ -8,12 +8,11 @@ import Flex from "../Styles/styledComponent/Flex";
 import Text from "../Styles/styledComponent/Text";
 
 function Nav() {
-  return React.createElement(
-    Flex,
-    {
-      align: "center",
-      justify: "between",
-      css: {
+  return (
+    <Flex
+      align="center"
+      justify="between"
+      css={{
         width: "100%",
         paddingTop: "$2",
         "& svg": {
@@ -33,116 +32,66 @@ function Nav() {
           },
         },
         animation: fadeinTop + " 1s ",
-      },
-    },
-    React.createElement(
-      Flex,
-      {
-        align: "center",
-        css: {
-          cursor: "pointer",
-        },
-      },
-      React.createElement(Ico_slogo, { width: "35", height: "35" }),
-      React.createElement(
-        Text,
-        {
-          cursor: "click",
-          css: {
-            headline4_i: "600",
-            marginLeft: "15px", //makes the logo text move from the logo graphic
-          },
-        },
-        React.createElement(Link, { to: "/" }, "Helping Hands")
-      )
-    ),
-    React.createElement(
-      Flex,
-      {
-        justify: "end",
-        align: "center",
-        css: {
-          "@bp2": {
-            display: "none",
-          },
-        },
-      },
-      React.createElement(
-        Flex,
-        {
-          css: {
+      }}
+    >
+      <Flex align="center" cursor="pointer">
+        <Ico_slogo width="35" height="35" />
+        <Text cursor="click" css={{ headline4_i: "600", marginLeft: "15px" }}>
+          <Link to="/">Helping Hands</Link>
+        </Text>
+      </Flex>
+      <Flex justify="end" align="center" css={{ "@bp2": { display: "none" } }}>
+        <Flex
+          css={{
             "& p": {
               headline6: "700",
               cursor: "pointer ",
               padding: "0 $3",
               color: "$onBg800",
-              "&:hover": {
-                color: "$onBg",
-              },
+              "&:hover": { color: "$onBg" },
             },
             marginRight: "$6",
-            "@bp2": {
-              marginRight: "$3",
-            },
-          },
-        },
-        React.createElement(
-          Link,
-          { to: "/volunteer" },
-          React.createElement(Text, null, "Volunteer")
-        ),
-        React.createElement(
-          Link,
-          { to: "/testimonials" },
-          React.createElement(Text, null, "Testimonials")
-        ),
-        React.createElement(
-          Link,
-          { to: "/about" },
-          React.createElement(Text, null, "About")
-        ),
-        React.createElement(
-          Link,
-          { to: "/register" },
-          React.createElement(Text, null, "Sign Up")
-        ),
-        React.createElement(
-          Link,
-          { to: "/signin" },
-          React.createElement(Text, null, "Sign In")
-        )
-      ),
-      React.createElement(
-        Flex,
-        {
-          justify: "around",
-          align: "center",
-          css: {
+            "@bp2": { marginRight: "$3" },
+          }}
+        >
+          <Link to="/volunteer">
+            <Text>Volunteer</Text>
+          </Link>
+          <Link to="/testimonials">
+            <Text>Testimonials</Text>
+          </Link>
+          <Link to="/about">
+            <Text>About</Text>
+          </Link>
+          <Link to="/register">
+            <Text>Sign Up</Text>
+          </Link>
+          <Link to="/signin">
+            <Text>Sign In</Text>
+          </Link>
+        </Flex>
+        <Flex
+          justify="around"
+          align="center"
+          css={{
             "& svg": {
               cursor: "pointer",
-              "&:hover": {
-                transform: "scale(1.1)",
-              },
+              "&:hover": { transform: "scale(1.1)" },
             },
             "& svg[id=userSvg]": {
               backgroundColor: "$primary600",
               fill: "$onPrimary",
               borderRadius: "$50",
               padding: "4px",
-              "&:hover": {
-                backgroundColor: "$primary",
-              },
+              "&:hover": { backgroundColor: "$primary" },
             },
-          },
-        },
-        React.createElement(Ico_user, {
-          width: "35",
-          height: "35",
-          id: "userSvg",
-        })
-      )
-    ),
-    React.createElement(Ico_menu, { width: "32", height: "32", id: "menu" })
+          }}
+        >
+          <Ico_user width="35" height="35" id="userSvg" />
+        </Flex>
+      </Flex>
+      <Ico_menu width="32" height="32" id="menu" />
+    </Flex>
   );
 }
 
