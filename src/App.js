849ce8e510567a2517083d235ca46d9c01150d30
Tuwan Ashnaz Sadiyan <ashnassadiyan@gmail.com>
2024-04-02 18:39:42 +0530

    added nav changes

--- src/App.js
+++ src/App.js
@@ -9,17 +9,22 @@ import {
 } from "./routes/routes";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { ORGANIZATION, VOLUNTEER } from "./constants";
 
 function App() {
-  // defaultRouter
-  // volunteerRoutes
-  // organizationRoutes
+  const getUser = JSON.parse(localStorage.getItem("user"));
+  const getRoutes =
+    getUser.type === VOLUNTEER
+      ? volunteerRoutes
+      : getUser.type === ORGANIZATION
+      ? organizationRoutes
+      : defaultRouter;
 
   globalStyle();
   return (
     <div style={{ width: "100%" }}>
       <Provider store={store}>
-        <RouterProvider router={volunteerRoutes} />
+        <RouterProvider router={getRoutes} />
       </Provider>
     </div>
   );
