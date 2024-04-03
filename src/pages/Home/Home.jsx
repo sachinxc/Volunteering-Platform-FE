18f719fac726b8cfc6be0dc67bbc3256da0adf91
sachinxc <sachincm10@gmail.com>
2024-04-03 15:50:54 +0530

    lp

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Typography, Grid } from "@mui/material";
-import BannerImage from "../../assets/LandingPageImages/background3.jpg";
+import BannerImage1 from "../../assets/LandingPageImages/background1.jpg";
+import BannerImage2 from "../../assets/LandingPageImages/background2.jpg";
+import BannerImage3 from "../../assets/LandingPageImages/background3.jpg";
 
 const Home = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const bannerImages = [BannerImage1, BannerImage2, BannerImage3];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex(
+        (prevIndex) => (prevIndex + 1) % bannerImages.length
+      );
+    }, 6000); // Change the image every 5 seconds (5000 milliseconds)
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <Grid
       container
       justifyContent="center"
       style={{
         minHeight: "100vh",
-        backgroundImage: `url(${BannerImage})`,
+        backgroundImage: `url(${bannerImages[currentImageIndex]})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
+        transition: "background-image 2s ease-in-out", // Apply transition effect
       }}
     >
       <Container>
