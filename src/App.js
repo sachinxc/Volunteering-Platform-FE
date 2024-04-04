53e43870b98ed1c373e45f089984f06c658232e8
sachinxc <sachincm10@gmail.com>
2024-04-04 07:12:51 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,3 @@
-/*import React from "react";
-import { RouterProvider } from "react-router-dom";
-// import globalStyle from "./Styles/globalCss";
-import {
-  defaultRouter,
-  volunteerRoutes,
-  organizationRoutes,
-} from "./routes/routes";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
-import { ORGANIZATION, VOLUNTEER } from "./constants";
-
-function App() {
-  const getUser = JSON.parse(localStorage.getItem("user"));
-  const getRoutes =
-    getUser.type === VOLUNTEER
-      ? volunteerRoutes
-      : getUser.type === ORGANIZATION
-      ? organizationRoutes
-      : defaultRouter;
-
-  // globalStyle();
-  return (
-    <div style={{ width: "100%" }}>
-      <Provider store={store}>
-        <RouterProvider router={volunteerRoutes} />
-      </Provider>
-    </div>
-  );
-}
-
-export default App;*/
-
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 // import globalStyle from "./Styles/globalCss";
