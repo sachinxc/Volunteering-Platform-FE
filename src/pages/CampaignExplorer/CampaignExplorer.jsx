7aa14f59894bf2e94667f9ee57da832e71ca7896
sachinxc <sachincm10@gmail.com>
2024-04-18 15:29:59 +0530

    ui

--- src/pages/CampaignExplorer/CampaignExplorer.jsx
+++ src/pages/CampaignExplorer/CampaignExplorer.jsx
@@ -34,7 +34,7 @@ const OpportunityCard = (props) => {
       sx={{
         borderRadius: "20px",
         border: "solid 1px black",
-        backgroundColor: "#e1f5ef",
+        backgroundColor: "#e6f7f0",
       }}
     >
       <Typography
